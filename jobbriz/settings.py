c7c8868c6bcc434c3ca94f5e0ce4a99c5e509281
Ratish149 <ratish.shakya149@gmail.com>
2024-12-26 11:49:08 +0545

    sd

--- jobbriz/settings.py
+++ jobbriz/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'jobbriz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobbriz',
+        'USER': 'jobbrizuser',
+        'PASSWORD': 'jobbriz@1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
